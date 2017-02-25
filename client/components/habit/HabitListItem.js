4ea39a8ac293b777562ee4e2ff181f7d51e2ebbb
ElitaSue <ElitaSue@gmail.com>
2017-02-25 14:06:04 -0700

    styling

--- client/components/habit/HabitListItem.js
+++ client/components/habit/HabitListItem.js
@@ -1,10 +1,26 @@
 import React from 'react';
-
 class HabitListItem extends React.Component {
   render() {
     return (
-      <li>
-        <div className="name">{this.props.habit.name}</div>
+        <li>
+          <div class="row">
+
+          <div className="card">
+            <div className="buttonArea">
+                <a className="btn-floating btn-med red">
+                <i className="material-icons">&#xE92B;</i></a>
+                <a className="btn-floating btn-med waves-effect waves-light red">
+                <i className="material-icons">&#xE86C;</i></a>
+          </div>
+          <div className="name">
+            {this.props.habit.name}
+          </div>
+          <div className="buttonArea2">
+          <button>EDIT</button>
+          </div>
+        </div>
+        </div>
+
       </li>
     );
   }
