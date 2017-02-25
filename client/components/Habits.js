76715b7a9a3118918612d25343c67e85c9d05a05
Justin Carter <justin@vidpresso.com>
2017-02-25 10:48:25 -0700

    habits list

--- client/components/Habits.js
+++ client/components/Habits.js
@@ -2,8 +2,22 @@ import React from 'react';
 
 class Habits extends React.Component {
   render() {
+    const habitListItems = [{name: "First"}, {name: "Second"}]
+
+    const habitList = habitListItems.map( habit => {
+      return (
+        <li>
+          <div className="name">{habit.name}</div>
+        </li>
+      )
+    })
+
     return (
-      <div>Habits List</div>
+      <div>
+        <ul>
+          { habitList }
+        </ul>
+      </div>
     );
   }
 };
